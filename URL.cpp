08a54474d53bbdef1b40e220938871a3f8d5affd
Sumit Kumar <sumitkn.pine@gmail.com>
2021-03-07 00:40:45 +0530

    URL in cpp program

---
+++ URL.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+	https://www.github.com/sumitk6497
+
+	int i;
+	for(i = 1; i <= 5; i++)
+	{
+	    std::cout<<"This code is error free......"<<std::endl;
+	}
+
+	return 0;
+}
