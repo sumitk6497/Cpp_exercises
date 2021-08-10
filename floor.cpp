e32121549fe5a9caa04bc1d22375a247a7c3e7d8
Sumit Kumar <sumitkn.pine@gmail.com>
2021-08-10 19:52:28 +0530

    floor() function

---
+++ floor.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	cout << "Floor is : " << floor(2.3) << endl;		//2
+	cout << "Floor is : " << floor(-2.3) << endl;		//-3
+	cout << "Floor is : " << floor(11.98) << endl;		//11
+
+
+	return 0;
+}
+
+//floor function returns largest integer smaller than or equal to x
