9b1ee690601df51f7ceed5c6773d8556d78c1261
Sumit Kumar <sumitkn.pine@gmail.com>
2021-08-10 19:51:58 +0530

    ceil() function

---
+++ ceil.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	cout << "Ceil is : " << ceil(2.3) << endl;		//3
+	cout << "Ceil is : " << ceil(-2.3) << endl;		//-2
+	cout << "Ceil is : " << ceil(11.98) << endl;		//12
+
+
+	return 0;
+}
+
+//ceil function returns smallest integer greater than or equal to x
