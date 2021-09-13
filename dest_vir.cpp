70bc9e40dcd7b339249527ca6e19f645a6083b6a
Sumit Kumar <sumitkn.pine@gmail.com>
2021-09-13 18:49:48 +0530

    Virtual Destructor

---
+++ dest_vir.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+class Base 
+{
+public:
+	Base() 
+	{ cout<<"Constructor: Base"<<endl; }
+	
+	virtual ~Base() 
+	{ cout<<"Destructor : Base"<<endl; }
+};
+
+class Derived: public Base 
+{
+public:
+	Derived() 
+	{ cout<<"Constructor: Derived"<<endl; }
+	
+	~Derived() 
+	{ cout<<"Destructor : Derived"<<endl; }
+};
+
+
+int main() 
+{
+	Base *Var = new Derived();
+	delete Var;
+
+	return 0;
+}
+
+/*
+Constructor: Base
+Constructor: Derived
+Destructor : Derived
+Destructor : Base
+*/
