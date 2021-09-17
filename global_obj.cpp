66f000ded4b79270572a57ef5705bbbd1be857d3
Sumit Kumar <sumitkn.pine@gmail.com>
2021-09-17 19:14:45 +0530

    Global & local object

---
+++ global_obj.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+class Test
+{
+public:
+	int i;
+	void get();
+};
+
+void Test::get()
+{
+	std::cout << "Enter the value of i: ";
+	std::cin >> i;
+}
+
+Test t;  // Global object
+
+int main()
+{
+	Test t;  // local object
+	t.get();
+	std::cout << "value of i in local t : " << t.i << std::endl;
+	::t.get();
+	std::cout << "value of i in global t: " << ::t.i << std::endl;
+	
+	return 0;
+}
