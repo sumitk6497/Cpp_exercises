faef42d3d1b9de10cc913e5f3bb8752a74d37334
Sumit Kumar <sumitkn.pine@gmail.com>
2021-09-23 22:34:23 +0530

    Constructor output question

---
+++ constr_6.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class Point 
+{
+	int x;
+public:
+	Point(int x) 
+	{ this->x = x; }
+
+	Point(const Point &p) 
+	{ x = p.x;}
+
+	int getX() 
+	{ return x; }
+};
+
+int main()
+{
+	Point p1(10);
+	Point p2 = p1;
+	cout << p1.getX() << endl;
+	cout << p2.getX() << endl;
+
+	return 0;
+}
