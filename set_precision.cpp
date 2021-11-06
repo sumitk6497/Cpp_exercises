47c7631a3bd523118395cbe84deab4941983e7fa
Sumit Kumar <sumitkn.pine@gmail.com>
2021-11-06 12:15:39 +0530

    set-precision

---
+++ set_precision.cpp
@@ -0,0 +1,13 @@
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << setprecision(17);
+	double d = 0.1;
+	cout << d << endl;			//0.10000000000000001
+
+	return 0;
+}
